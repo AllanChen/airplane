c14a2156259726a1ada2696996e7de6401b1559f
ChanAllan <allanchanni@gmail.com>
2015-02-25 13:46:48 +0800

    Games
    Games

---
+++ Assets/Script/CharacterScript/CharacterMove.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CharacterMove : MonoBehaviour {
+
+	Animator anitor;
+	float hor;
+	float ver;
+
+	int warkingHash = Animator.StringToHash("Base Layer.Walk");
+	int runHash 	= Animator.StringToHash("Base Layer.Run");
+	
+	//public AudioClip 	audioClip;
+
+	void Start () {
+		anitor = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hor = Input.GetAxis("Horizontal");
+		ver = Input.GetAxis("Vertical");
+
+
+	}
+
+	void FixedUpdate()
+	{
+		anitor.SetFloat("walking",ver);
+		
+		AnimatorStateInfo  animationStateInfo = anitor.GetCurrentAnimatorStateInfo(0);
+		
+		if(Input.GetKeyDown(KeyCode.LeftShift) && animationStateInfo.nameHash == warkingHash)
+		{
+			anitor.SetBool("run",true);
+		}
+		
+		if(animationStateInfo.nameHash == runHash && Input.GetKeyUp(KeyCode.LeftShift) )
+		{
+			anitor.SetBool("run",false);
+		}
+		
+		if(animationStateInfo.nameHash == runHash && Input.GetKeyDown(KeyCode.Space))
+		{
+			anitor.SetBool("runToJump",true);
+		}
+		
+		if(animationStateInfo.nameHash == runHash && Input.GetKeyUp(KeyCode.Space))
+		{
+			anitor.SetBool("runToJump",false);
+		}
+		
+		if(animationStateInfo.nameHash == warkingHash && Input.GetKeyDown(KeyCode.Space))
+		{
+			anitor.SetBool("walkToJump",true);
+		}
+		
+		if(animationStateInfo.nameHash == warkingHash && Input.GetKeyUp(KeyCode.Space))
+		{
+			anitor.SetBool("walkToJump",false);
+		}
+
+	}
+}
